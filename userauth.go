5c906827412fc8f2e1936aa920225cb5b51b4ac6
Okeanalysis <emmanueloke760@gmail.com>
2025-04-07 00:20:44 +0100

    update

--- userauth.go
+++ userauth.go
@@ -3,42 +3,58 @@ package main
 import (
 	"net/http"
 	"time"
-	"github.com/dgrijalva/jwt-go"
+
 	"github.com/gin-gonic/gin"
-	"golang.org/x/crypto/bcrypt"
 	"github.com/jmoiron/sqlx"
-	"github.com/lib/pq"
+	"golang.org/x/crypto/bcrypt"
 )
 
-
-type User struct{
-	ID int `db:"id" json:"id"`
-	Email string `db:"email" json:"email"`
-	PasswordHash string `db:"password_hash" json:"-"`
-	CreatedAt time.Time `db:"created_at" json:"created_at"`
-	UpdatedAt time.Time `db:updated_at json:updated_at`
+type User struct {
+	ID           int       `db:"id" json:"id"`
+	Email        string    `db:"email" json:"email"`
+	PasswordHash string    `db:"password_hash" json:"-"`
+	CreatedAt    time.Time `db:"created_at" json:"created_at"`
+	UpdatedAt    time.Time `db:updated_at json:updated_at`
 }
 
-var DB* sqlx.DB
+var DB *sqlx.DB
 
 func initDB() {
 	var err error
-	DB, err= sqlx.connect("postgres", "user= postgres dbname=database sslmode=disable")
-	if err !=nil{
+	DB, err = sqlx.connect("postgres", "user= postgres dbname=database sslmode=disable")
+	if err != nil {
 		panic(err)
 	}
 
 }
 
-const jwtSecret= "eehicarburbur"
+const jwtSecret = "eehicarburbur"
 
 func Register(c *gin.Context) {
-	var user User 
-	if err:= c.ShouldBindJSON(&user); err!=nil{
+	var user User
+	if err := c.ShouldBindJSON(&user); err != nil {
 		c.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
 		return
 	}
 
-	hashedpassword, _:= 
-bcrypt.GenerateFromPassword([]byte(user.PasswordHash)),
+	hashedPassword, _ :=
+		bcrypt.GenerateFromPassword([]byte(user.PasswordHash),
+			bcrypt.DefaultCost)
+	user.PasswordHash = string(hashedPassword)
+
+	_, err := DB.NamedExec(`INSERT INTO users (email, password_hash)
+VALUES (:email, :password_hash)`, user)
+	if err != nil {
+		c.JSON(http.StatusInternalServerError, gin.H{"error": "User already exists"})
+		return
+	}
+	c.JSON(http.StatusCreated, gin.H{"message": "User registered succesfully"})
+
+}
+
+func Login(c *gin.Context) {
+	var user User
+	if err := c.ShouldBindBodyWithJSON(&user); err != nil {
+		c
+	}
 }
